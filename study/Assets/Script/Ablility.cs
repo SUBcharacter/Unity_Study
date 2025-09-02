2af3db8ac4265359297515d48be14adc1b87adc6
SUBcharacter <farg557@gmail.com>
2025-09-02 15:40:58 +0900

    2025.09.02 수출용 패키지

---
+++ study/Assets/Script/Ablility.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+
+    public int ATK = 40;
+
+    void Start()
+    {
+        Debug.Log($"ATK : {ATK}");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
